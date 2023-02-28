fcfa75adfb070fc03d25aaa44744ae8b07930a39
unknown <martinsogunsina@gmail.com>
2023-02-28 10:26:45 +0600

    changes

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,31 +1,31 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import { auth } from '../firebase';
+import React, { useContext, useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { auth } from "../firebase";
 
 const AuthContext = React.createContext();
 
 export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
-    const [loading, setloading] = useState(true);
-    const [user, setUser] = useState(null);
-    console.log({user})
-    const history = useHistory();
+  const [loading, setloading] = useState(true);
+  const [user, setUser] = useState(null);
+  console.log({ user });
+  const history = useHistory();
 
-    useEffect(() => {
-        auth.onAuthStateChanged((user) => {
-            setUser(user);
-            setloading(false);
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setUser(user);
+      setloading(false);
 
-            if(user) history.push('/chats');
-        })
-    }, [user, history]);
- 
-    const value = { user };
+      if (user) history.push("/chats");
+    });
+  }, [user, history]);
 
-    return (
-        <AuthContext.Provider value={value}>
-            {!loading && children}
-        </AuthContext.Provider>
-    )
-}
+  const value = { user };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,13 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 
-export const auth = firebase.initializeApp({
+export const auth = firebase
+  .initializeApp({
     apiKey: "AIzaSyDnDsoCbRbET1VnoCHCrTcv9_CaZepOY-8",
     authDomain: "nazochat-1dc2f.firebaseapp.com",
     projectId: "nazochat-1dc2f",
     storageBucket: "nazochat-1dc2f.appspot.com",
     messagingSenderId: "1037431702372",
-    appId: "1:1037431702372:web:ec6f921c4c0d7545fc7071"
-  }).auth();
+    appId: "1:1037431702372:web:ec6f921c4c0d7545fc7071",
+  })
+  .auth();
